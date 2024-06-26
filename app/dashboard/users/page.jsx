1c8cf063f34bda7cc2f6ccbf0a0401fdddd4c49c
dr-dahou-adrar <dahou.abdghani@univ-adrar.edu.dz>
2024-06-26 11:52:51 +0800

    up

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,63 @@
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Link from "next/link";
+import Image from "next/image";
+
 const UsersPage = () => {
     return ( 
-        <div>UsersPage</div>
-     );
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search users ..." />
+
+                <Link href="/dashboard/users/add"> 
+                <button className={styles.addButton}> Add New </button>
+                </Link> 
+
+            </div>
+            <table className={styles.table}>
+                <head>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Created At</td>
+                        <td>Role</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </head>
+                <tbody>
+                    <tr>
+                        <td> 
+                            <div className={styles.user}>
+                            <Image
+                                src="/noavatar.png"
+                                alt=""
+                                width={40}
+                                height={40}
+                                className={styles.userImage}
+                            />
+                            John Doe
+                            </div>
+                        </td>
+                        <td>john@gmail.com</td>
+                        <td>24.06.2024</td>
+                        <td>Admin</td>
+                        <td>Active</td>
+                        <td>
+                            <Link href="/">
+                                <button className={`${styles.button} ${styles.view}`} > View </button>
+                            </Link>
+                            <button className={`${styles.button} ${styles.delete}`} > Delete </button>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination/>
+        </div>
+
+    );
 }
  
 export default UsersPage;
---
+++ app/ui/dashboard/users/users.jsx
@@ -0,0 +1,7 @@
+const Users = () => {
+    return ( 
+        <div>USERS content</div>
+     );
+}
+ 
+export default Users;
